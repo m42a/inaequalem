d745d11bc7e2e86375e464166527d56c4e7019c1
Marc Aldorasi <m101010a@gmail.com>
2011-10-07 15:04:42 -0400

    fps counter

--- inaequalem.cpp
+++ inaequalem.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <algorithm>
+#include <ctime>
 
 #include "inaequalem.h"
 #include "bullet.h"
@@ -16,7 +17,10 @@ const float textlineheight=119.05+33.33; //From the documentation
 //static bool directions[4]={false,false,false,false}; //Up down left right
 int direction=0;
 
+struct timespec then;
 int ticker=0;
+string fps;
+
 player p(.5, .5);
 
 void writetext(float x, float y, float size, const string &s)
@@ -39,7 +43,8 @@ void drawSidepanel()
 	glColor3f(0.2, 0.2, 0.2);
 	glRectf(1,0,ratio,1);
 	glColor3f(1.0, 1.0, 1.0);
-	writetext(1.1, .9, .05, "Score");
+	writetext(1.02, .9, .05, "Score");
+	writetext(1.02, .02, .03, fps);
 }
 
 void render()
@@ -86,6 +91,19 @@ void gamelogic(int v)
 	else if (direction==-4)
 		p.move(player::downright);
 	++ticker;
+	if (ticker%32==0)
+	{
+		struct timespec now;
+		clock_gettime(CLOCK_MONOTONIC, &now);
+		double diff=(now.tv_sec-then.tv_sec)+(now.tv_nsec-then.tv_nsec)/1000000000.0;
+		double frames=32/diff;
+		int size=snprintf(NULL, 0, "FPS: %05.2f", frames);
+		char *s=new char[size+1];
+		sprintf(s, "FPS: %05.2f", frames);
+		fps=s;
+		delete[] s;
+		then=now;
+	}
 	glutPostRedisplay();
 }
 
@@ -107,7 +125,6 @@ void specialdown(int key, int x, int y)
 		direction+=1;
 	else if (key==GLUT_KEY_RIGHT)
 		direction-=1;
-	//direction=3*(directions[0]-directions[1])+directions[2]-directions[3];
 }
 
 void specialup(int key, int x, int y)
@@ -129,9 +146,11 @@ void mousemove(int x, int y)
 int main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(640, 480);
 	glutCreateWindow("Not touhou");
+
+	clock_gettime(CLOCK_MONOTONIC, &then);
 
 	glClearColor(1,1,1,0);
 	glEnable(GL_BLEND);
